4f0033eacc44afa76c95b97973e34a35b3ef50fc
Brian Morrison II <brian@brianmorrison.me>
2022-02-09 14:51:47 -0600

    🔊  (backend) Added logging to opengraph handler

--- backend/stack/s3-handlers/create-og-image/main.go
+++ backend/stack/s3-handlers/create-og-image/main.go
@@ -28,6 +28,7 @@ var bgbytes []byte
 func handler(ctx context.Context, s3Event events.S3Event) {
 	for _, record := range s3Event.Records {
 		s3 := record.S3
+		log.Println(fmt.Sprintf("(handler) Handling build %v", s3.Object.Key))
 		createImage(s3.Bucket.Name, s3.Object.Key)
 		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
 	}
