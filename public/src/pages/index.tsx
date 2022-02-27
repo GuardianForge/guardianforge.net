be3ec8fcad9eaf64909e7c4de44bdd61c1a83087
Brian Morrison II <brian@brianmorrison.me>
2022-02-27 13:53:45 -0600

    ✨  Rebuild home page

--- public/src/pages/index.tsx
+++ public/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useContext } from "react"
 import styled from "styled-components"
-import HomeInfoCard from "../components/HomeInfoCard"
 import BuildSummaryCard from "../components/app/BuildSummaryCard"
 // import BuildSummaryCard from "../components/BuildSummaryCard"
 // @ts-ignore
@@ -8,9 +7,13 @@ import LogoNoBg from "../images/logo-no-bg.png"
 import Loading from "../components/app/Loading"
 import { GlobalContext } from "../contexts/GlobalContext"
 import { Helmet } from "react-helmet"
-import { Alert } from "react-bootstrap"
-import { Link } from "gatsby"
+import { Alert, Col, Container, Row } from "react-bootstrap"
+import { Link, navigate } from "gatsby"
 import BuildSummary from "../models/BuildSummary"
+import ForgeButton from "../components/app/forms/Button"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCube, faDonate, faGem, faSignInAlt, faUser } from "@fortawesome/free-solid-svg-icons"
+import colors from "../colors"
 
 // styles
 const GuardianForgeImage = styled.img`
@@ -18,9 +21,19 @@ const GuardianForgeImage = styled.img`
   margin: 0 auto;
 `
 
+const Wrapper = styled(Container)`
+  .home-gif {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border-radius: 10px;
+    border: 2px solid ${colors.theme2.accent2};
+    max-width: 300px;
+  }
+`
+
 // markup
 const IndexPage = () => {
-  const { isConfigLoaded, dispatchAlert } = useContext(GlobalContext)
+  const { isConfigLoaded, dispatchAlert, redirectToLogin } = useContext(GlobalContext)
   const [isLoading, setIsLoading] = useState(true)
   const [latestBuilds, setLatestBuilds] = useState([])
 
@@ -58,7 +71,7 @@ const IndexPage = () => {
   }, [isConfigLoaded])
 
   return (
-    <div id="home" className="container">
+    <Wrapper id="home">
       <Helmet>
         <title>GuardianForge</title>
       </Helmet>
@@ -78,24 +91,58 @@ const IndexPage = () => {
         </Alert>
       </div>
 
-      <div className="row py-5 my-5">
-        <h2 className="col-md-12">How it works</h2>
-        <div className="col-md-4">
-          <HomeInfoCard icon="search" title="Search.">
-            Search for a user within the Destiny userbase, or sign in to see your own Guardians!
-          </HomeInfoCard>
-        </div>
-        <div className="col-md-4">
-          <HomeInfoCard icon="cube" title="Build.">
-            Highlight your Guardian's best gear, specify your play style, and explain why it works together.
-          </HomeInfoCard>
-        </div>
-        <div className="col-md-4">
-          <HomeInfoCard icon="share" title="Share.">
-            Easily share your build with your friends, fireteam, or anyone in the world!
-          </HomeInfoCard>
-        </div>
-      </div>
+      <Row className="py-5 my-5 flex-lg-row-reverse align-items-center">
+        <Col md="8">
+          <div className="px-2">
+            <h2>Find Community Builds</h2>
+            <p>
+              GuardianForge provides a simple way to find the builds you are looking for! Filter by Class, Subclass, Activity, or simply search for a specific piece of gear.
+            </p>
+            <p>
+              Once you find a build, you can bookmark it for later (logged in users only), share it with your friends & viewers, or create a DIM Loadout Optimizer link to make it your own!
+            </p>
+            <ForgeButton onClick={() => navigate("/find-builds")}><FontAwesomeIcon icon={faCube} /> Find Builds</ForgeButton>
+          </div>
+        </Col>
+        <Col className="" md="4">
+          <img className="img-fluid home-gif" src="/img/gifs/find-builds.gif" />
+        </Col>
+      </Row>
+
+      <Row className="py-5 my-5">
+        <Col md="8">
+          <div className="px-2">
+            <h2>Create Your Own Builds</h2>
+            <p>
+              The power of GuardianForge lies in allowing you to create builds based on any user in the Destiny player-base. Search for players to view their current Guardian loadout and create snapshots to share.
+            </p>
+            <p>
+              Sign into Forge using your Bungie account to unlock even more features like viewing your build history, bookmarked builds, and creating custom build directly in the app without having to set up your Guardian in-game!
+            </p>
+            <div>
+              <ForgeButton onClick={() => navigate("/find-players")} style={{marginRight: "10px"}}><FontAwesomeIcon icon={faUser} /> Find Players</ForgeButton>
+              <ForgeButton onClick={() => redirectToLogin()}><FontAwesomeIcon icon={faSignInAlt} /> Login w/Bungie</ForgeButton>
+            </div>
+          </div>
+        </Col>
+        <Col md="4">
+          <img className="img-fluid home-gif" src="/img/gifs/make-build.gif" />
+        </Col>
+      </Row>
+
+      {/* <Row className="py-5 my-5">
+        <Col md="8">
+          <h2>Support GuardianForge</h2>
+          <p>
+            GuardianForge is the product of one developer and many hours of development time. You can help keep it online by subscribing to remove ads, or making a one-time donation below!
+          </p>
+          <ForgeButton onClick={() => navigate("/find-builds")} style={{marginRight: "10px"}}><FontAwesomeIcon icon={faGem} /> Subscribe</ForgeButton>
+          <ForgeButton onClick={() => navigate("/find-builds")}><FontAwesomeIcon icon={faDonate} /> Donate</ForgeButton>
+        </Col>
+        <Col md="4">
+          <div>gif or image here</div>
+        </Col>
+      </Row> */}
 
 
       <div className="row">
@@ -112,7 +159,7 @@ const IndexPage = () => {
           </>
         )}
       </div>
-    </div>
+    </Wrapper>
   )
 }
 
