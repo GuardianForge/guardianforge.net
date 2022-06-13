c1ffd8bc2f0d0dfe09d024e69b8a05695c96955b
Brian Morrison II <brian@brianmorrison.me>
2022-06-13 17:13:51 -0500

    Blog - Update PWA theme color

--- frontend/blog/gatsby-config.js
+++ frontend/blog/gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
         short_name: `GatsbyJS & WP`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
       },
