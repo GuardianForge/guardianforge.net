b6eed936e2fabcaabb0dbfc6b8c78663a03aab8d
Brian Morrison II <brian@brianmorrison.me>
2022-09-16 14:58:45 -0500

    disable local config to get blog and docs to build

--- frontend/blog/build.config.js
+++ frontend/blog/build.config.js
@@ -2,9 +2,9 @@ const fs = require("fs")
 
 let config = require("./build.config.json")
 
-if(fs.existsSync("./build.config.dev.json")) {
-  let devConfig = require("./build.config.dev.json")
-  config = devConfig
-}
+// if(fs.existsSync("./build.config.dev.json")) {
+//   let devConfig = require("./build.config.dev.json")
+//   config = devConfig
+// }
 
 module.exports = config
