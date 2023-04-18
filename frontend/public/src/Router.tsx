61050405a1447a69f8d84c214b68e62410daf67a
Brian Morrison II <brian@brianmorrison.me>
2023-04-17 21:18:22 -0500

    fix routing, remove app files

--- frontend/public/src/Router.tsx
+++ frontend/public/src/Router.tsx
@@ -4,25 +4,13 @@ import FindBuilds from './views/FindBuilds'
 import FindPlayers from './views/FindPlayers'
 import Home from './views/Home'
 import OAuthHandler from './views/OAuthHandler'
-import PublicGuardian from './views/PublicGuardian'
 import PublicProfile from './views/PublicProfile'
-import AppMain from './views/app/AppMain'
-import AppFindPlayers from './views/app/FindPlayers'
-import AppFindBuilds from './views/app/FindBuilds'
-import Bookmarks from './views/app/Bookmarks'
 import BookmarksMain from './views/Bookmarks'
 import MainBookmarks from './views/Bookmarks'
-import UserProfile from './views/app/UserProfile'
 import GuardianProfileMain from './views/GuardianProfile'
-import GuardianProfile from './views/app/GuardianProfile'
-import AppBuild from './views/app/Build'
-import MyBuilds from './views/app/MyBuilds'
 import MyBuildsMain from './views/MyBuilds'
-import CreateBuild from './views/app/CreateBuild'
 import CreateBuildMain from './views/CreateBuild'
-import EditProfile from './views/app/EditProfile'
 import EditProfileMain from './views/EditProfile'
-import AdminTools from './views/app/admin/AdminTools'
 import AdminToolsMain from './views/AdminTools'
 import NotFound from './views/NotFound'
 import About from './views/About'
@@ -74,22 +62,13 @@ function ForgeRouter() {
         <Route path="/u/:username" element={<PublicProfile />} />
         <Route path="/g/:guardianKey" element={<GuardianProfileMain />} />
 
-
-        {/* <Route path="/app" element={<AppMain />} />
-        <Route path="/app/find-players" element={<AppFindPlayers />} />
-        <Route path="/app/find-builds" element={<AppFindBuilds />} />
-        <Route path="/app/bookmarks" element={<Bookmarks />} />
-        <Route path="/app/my-builds" element={<MyBuilds />} />
-        <Route path="/app/create-build" element={<CreateBuild />} />
-        <Route path="/app/u/:username" element={<UserProfile />} />
-        <Route path="/app/g/:guardianKey" element={<GuardianProfile />} />
-        <Route path="/app/build/:buildId" element={<AppBuild />} />
-        <Route path="/app/edit-profile" element={<EditProfile />} />
-        <Route path="/app/admin/admin-tools" element={<AdminTools />} /> */}
-
         {/* Redirects */}
         <Route path="/blog" element={<Redirect to="/blog/index.html" />} />
         <Route path="/docs" element={<Redirect to="/docs/index.html" />} />
+
+        {/* App route fixes */}
+        <Route path="/app" element={<Redirect to="/" />} />
+        <Route path="/app/*" element={<Redirect to="/" />} />
 
         {/* 404s */}
         <Route path="*" element={<NotFound />} />
