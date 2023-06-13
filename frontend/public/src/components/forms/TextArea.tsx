b0a4cae7a5b1d0d33c3d92894dff4f33a345642c
Brian Morrison II <brian@planetscale.com>
2023-06-12 22:47:34 -0400

    hide input outlines

--- frontend/public/src/components/forms/TextArea.tsx
+++ frontend/public/src/components/forms/TextArea.tsx
@@ -1,65 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import colors from "../../colors"
-
-const Wrapper = styled.div`
-  color: #eee;
-  background-color: ${colors.theme2.dark3};
-  border-radius: 5px;
-  box-shadow: 5px 5px rgba(0,0,0,0.1);
-  padding: 10px;
-
-  .main {
-    display: flex;
-    justify-content: space-between;
-
-    textarea {
-      color: #eee;
-      background-color: inherit !important;
-      border: 0;
-      width: 100%;
-      height: auto;
-
-      &:focus {
-        outline: none;
-      }
-    }
-
-    .left {
-      display: flex;
-      align-items: top0;
-      flex: 1;
-
-      svg {
-        color: ${colors.theme2.dark1};
-        margin: 5px 3px;
-      }
-
-      input {
-        flex: 1
-      }
-    }
-
-    .right {
-      display: flex;
-
-      .submit-button {
-        color: #eee;
-        background-color: #006eff !important;
-        border: 0;
-        border-radius: 5px;
-        width: 50px;
-      }
-    }
-  }
-
-  .gutter {
-    display: flex;
-    flex-flow: row wrap;
-  }
-`
 
 type Props = {
   value: string
@@ -88,7 +29,7 @@ function TextArea(props: Props) {
         <div className="flex items-start gap-1 flex-1">
           {prefixIcon && <FontAwesomeIcon className='text-neutral-500 mt-1' icon={prefixIcon} />}
           <textarea placeholder={placeholder ? placeholder : ""}
-            className='bg-transparent flex-1'
+            className='bg-transparent flex-1 focus:outline-none focus:border-transparent focus:ring-0 focus-visible:outline-none focus-visible:border-transparent focus-visible:ring-0'
             rows={rows ? rows : 3}
             value={value}
             onChange={onChangeHandler} />
